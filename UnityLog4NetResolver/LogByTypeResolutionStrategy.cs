80c0fedec3734181c823154f5459b9a9d18f472f
stop-cran <stop-cran@list.ru>
2018-11-21 16:27:14 +0200

    Version 1.0.1

---
+++ UnityLog4NetResolver/LogByTypeResolutionStrategy.cs
@@ -0,0 +1,17 @@
+﻿using log4net;
+using Unity.Builder;
+using Unity.Builder.Strategy;
+
+namespace UnityLog4NetResolver
+{
+    internal sealed class LogByTypeResolutionStrategy : BuilderStrategy
+    {
+        public override void PreBuildUp(IBuilderContext context)
+        {
+            if (context.OriginalBuildKey.Type == typeof(ILog) && context.ParentContext != null)
+                context.Existing = LogManager.GetLogger(context.ParentContext.BuildKey.Type);
+
+            base.PreBuildUp(context);
+        }
+    }
+}
---
+++ UnityLog4NetResolver/LogByTypeResolverExtension.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Builder;
+using Unity.Extension;
+
+namespace UnityLog4NetResolver
+{
+    public class LogByTypeResolverExtension : UnityContainerExtension
+    {
+        protected override void Initialize() =>
+            Context.Strategies.Add(new LogByTypeResolutionStrategy(), UnityBuildStage.PreCreation);
+    }
+}
